5efec7ba5fffbc008973566770b9fda303b873f7
tanaka5901 <36415974+tanaka5901@users.noreply.github.com>
2020-07-17 15:18:48 +0900

    Revert "Feature/output csv file for purchases"

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\OfficeStockService;
-use App\Services\OfficePurchasesService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('OfficeStockService', OfficeStockService::class);
-        $this->app->bind('OfficePurchasesService', OfficePurchasesService::class);
+        //
     }
 
     /**
